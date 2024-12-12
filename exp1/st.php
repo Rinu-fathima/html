18499154f0223d2b1bba08a217dbaa64f49ddcf6
Rinu-fathima <24mp2291@rit.ac.in>
2024-12-12 09:56:26 +0530

    Add files via upload

---
+++ exp1/st.php
@@ -0,0 +1,7 @@
+<?php
+$name=$_POST['name'];
+$class=$_POST['class'];
+$age=$_POST['age'];
+$admn_no=$_POST['admn_no'];
+echo"Name of student:$name <br> Class:$class <br> age:$age <br> Admission number:$admn_no";
+?>
